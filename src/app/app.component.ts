8d5722f3c0de1722bbe30d369e7f45282398b606
fazd <fabioz@uninorte.edu.co>
2020-05-03 02:48:58 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,108 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hand-written-ui';
+  
+
+  public canvas:any;
+  public card: any;
+  public ctx:any;
+  public flag = false;
+  public prevX = 0;
+  public currX = 0;
+  public prevY = 0;
+  public currY = 0;
+  public dot_flag = false;
+  public widthLine = 0;
+  public width = 0;
+  public height = 0;
+  public selected= '';
+  
+  ngOnInit() {
+    this.canvas = document.getElementById('mycanvas');  
+    this.ctx = this.canvas.getContext("2d");
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+  }
+  
+  getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+  mouseMove(event ){
+    
+    this.findxy('move', event);
+  }
+   
+  mouseDown(event){
+    this.findxy('down', event);
+  }
+
+  mouseUp(event){
+    this.findxy('up', event);
+  }
+
+  mouseLeave(event){
+    this.findxy('out', event);
+  }
+
+
+  draw() {
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.prevX, this.prevY);
+      this.ctx.lineTo(this.currX, this.currY);
+      this.ctx.lineWidth = this.selected;
+      this.ctx.strokeStyle = '#ffffff';
+      this.ctx.stroke();
+      this.ctx.closePath();
+  }
+    
+  erase() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    
+  }
+
+  findxy(res, e) {
+    //console.log('(',e.clientX,',',e.clientY,')');
+    //console.log('(',this.canvas.offsetLeft,',',this.canvas.offsetTop,')');
+    if (res == 'down') {
+        this.prevX = this.currX;
+        this.prevY = this.currY;
+        const a = this.getMousePos(this.canvas,e);
+        this.currX = a.x;
+        this.currY = a.y;
+        console.log('(',this.currX,',',this.currY,')');
+        this.flag = true;
+        this.dot_flag = true;
+        if (this.dot_flag) {
+          this.ctx.beginPath();
+          this.ctx.fillRect(this.currX, this.currY, 2, 2);
+          this.ctx.closePath();
+          this.dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+      this.flag = false;
+    }
+    if (res == 'move') {
+      if (this.flag) {
+        this.prevX = this.currX;
+        this.prevY = this.currY;
+        const a = this.getMousePos(this.canvas,e);
+        this.currX = a.x;
+        this.currY = a.y;
+        this.draw();
+      }
+    }
+  }
+
+  create(){
+    const data = this.canvas.toDataURL();
+    console.log(data);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+//import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+//import { faGithub } from '@fortawesome/free-brands-svg-icons';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatChipsModule,
+    //FontAwesomeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  /*constructor(library: FaIconLibrary) {
+    // library.addIconPacks(fas);
+    library.addIcons(
+      faGithub
+    );
+  }*/
+
+
+ }
